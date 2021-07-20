ddc71e98707a17442f54e8f21e7758259194d761
jk-engineer <jk-218@yandex.ru>
2021-07-20 20:37:53 +0300

    Add license header

--- XboxSeriesX_Eldorado.py
+++ XboxSeriesX_Eldorado.py
@@ -1,5 +1,18 @@
 # Программа для мониторинга наличия Xbox Series X в интернет-магазине Эльдорадо
+# Copyright (C) 2021 Evgeniy Ipatov
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import random
 import requests
